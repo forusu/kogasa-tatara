ad98a66cf2618938790df78e475acc2656008d53
isakkr <isakkr@sbb.rs>
2022-08-26 03:14:56 +0200

    Limited avatar size

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,12 +4,12 @@ exports.run = (client, message, args, user) => {
     const mention = message.mentions.users.first();
 
     if (!mention) {
-        imageurl = message.author.avatarURL
+        imageurl = message.author.avatarURL.slice(0, message.author.avatarURL.indexOf('?')) +"?size=256"
     } else if (mention && args[1]) {
         message.react('❌')
         return message.channel.send("☂ You can only request an avatar from one user!")
     } else if (mention) {
-        imageurl = mention.avatarURL
+        imageurl = mention.avatarURL.slice(0, mention.avatarURL.indexOf('?')) + "?size=256"
     }
 
     message.channel.send({embed: {
