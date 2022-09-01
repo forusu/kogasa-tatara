ac695c6434726aa69b1f053232306e63dbe1117a
isakkr <isakkr@sbb.rs>
2022-09-01 17:52:50 +0200

    Fixed user join date

--- commands/user.js
+++ commands/user.js
@@ -1,4 +1,4 @@
-const { User, MessageFlags } = require("discord.js")
+const { User, MessageFlags, GuildMember } = require("discord.js")
 const moment = require("moment");
 require("moment-duration-format");
 
@@ -12,17 +12,18 @@ exports.run = (client, message, args, user) => {
     let userJoinDate
 
     const mention = message.mentions.users.first();
+    const mentionMember = message.mentions.members.first();
 
     if (!mention) {
         imageurl = message.author.avatarURL()
         userTag = message.author.tag
         uname = message.author.username
         userId = message.author.id
-        userJoinDate = moment(message.author.joinedAt).format("DD/MM/YYYY HH:mm:ss")
+        userJoinDate = moment(message.member.joinedTimestamp).format("DD/MM/YYYY HH:mm:ss")
         userCreationDate = moment(message.author.createdTimestamp).format("DD/MM/YYYY HH:mm:ss")
 
     } else if (mention && args[1]) {
-
+        console.log(mention)
         message.react('❌')
         return message.channel.send("☂ You can only request user info from one user!")
 
@@ -32,7 +33,7 @@ exports.run = (client, message, args, user) => {
         uname = mention.username
         userTag = mention.tag
         userId = mention.id
-        userJoinDate = moment(mention.joinedAt).format("DD/MM/YYYY HH:mm:ss")
+        userJoinDate = moment(mentionMember.joinedTimestamp).format("DD/MM/YYYY HH:mm:ss")
         userCreationDate = moment(mention.createdTimestamp).format("DD/MM/YYYY HH:mm:ss")
 
     }
